d3cdbf5a4d908f3f2db714aa97158d60e8728c86
dejiborewa <ayodejiakinborewa@gmail.com>
2023-01-11 01:39:54 +0100

    fix: dummy deploy

--- components/availableTrips/availableTrips.tsx
+++ components/availableTrips/availableTrips.tsx
@@ -1,19 +1,36 @@
+import { useRouter } from "next/router";
 import { FC } from "react";
 import Add from "../add/add";
 import Trip from "../trip/trip";
 import AvailableTripsProps from "./availableTrips.props";
 
 const AvailableTrips: FC<AvailableTripsProps> = ({ trips }) => {
+  const router = useRouter();
+
+  const role = "ADMIN";
+
   return (
     <>
       <section className="mt-4">
         {trips.map((trip, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            onClick={
+              role === "ADMIN"
+                ? () => router.push("/trip/edit-trip")
+                : () => console.log("nothing")
+            }
+          >
             <Trip data={trip} />
           </div>
         ))}
       </section>
-      <Add />
+      <div
+        className="my-16 w-max mx-auto"
+        onClick={() => router.push("/create-trip")}
+      >
+        <Add />
+      </div>
     </>
   );
 };
--- components/progressBar/progressBar.tsx
+++ components/progressBar/progressBar.tsx
@@ -3,25 +3,24 @@ import { FC } from "react";
 import ProgressBarProps from "./progressBar.props";
 
 const ProgressBar: FC<ProgressBarProps> = ({ state, shadow }) => {
-  // console.log(state);
   const router = useRouter();
   return (
     <div className="flex justify-center mt-6 gap-2 absolute bottom-0 mb-8 left-1/2 -translate-x-1/2">
       <div
-        className={`bg-grey w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
-          state === 1 ? "bg-customYellow w-[76px] h-[14px}" : ""
+        className={`w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
+          state === 1 ? "bg-customYellow w-[76px] h-[14px}" : "bg-grey"
         }`}
         onClick={() => router.push("/onboarding/1")}
       ></div>
       <div
-        className={`bg-grey w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
+        className={`w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
           shadow ? "shadow-red" : ""
-        } ${state === 2 ? "bg-customYellow w-[76px] h-[14px}" : ""}`}
+        } ${state === 2 ? "bg-customYellow w-[76px] h-[14px}" : "bg-grey"}`}
         onClick={() => router.push("/onboarding/2")}
       ></div>
       <div
-        className={`bg-grey w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
-          state === 3 ? "bg-customYellow w-[76px] h-[14px}" : ""
+        className={`w-[34px] h-[14px] rounded-[7px] cursor-pointer ${
+          state === 3 ? "bg-customYellow w-[76px] h-[14px}" : "bg-grey"
         }`}
         onClick={() => router.push("/onboarding/3")}
       ></div>
